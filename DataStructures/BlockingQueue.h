25cd351ab1748a5b4fdd5efe6dffdf415d1fa075
unknown <zhch@Zhong-Jackets.redmond.corp.microsoft.com>
2015-05-16 21:05:39 -0700

    add more files

---
+++ DataStructures/BlockingQueue.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+namespace clock18
+{
+
+// The blocking queue is used in the consumer-producer scenario.
+// The push, pop, size should be thread-safe.
+class BlockingQueue
+{
+public:
+
+private:
+
+};
+
+}
---
+++ UnitTests/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+ 
+int main(char* argc, char** argv)
+{
+    cout << 0x80 << endl;
+    cout << pow(2,  7) << endl;
+
+    string str = "abc";
+    str.erase(1,1);
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
